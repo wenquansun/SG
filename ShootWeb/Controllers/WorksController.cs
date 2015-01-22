70a6ff36c3a235ccd650cb7da31991d8c6c5077a
wenquansun <604164227@qq.com>
2015-01-22 17:21:02 +0800

    完善

---
+++ ShootWeb/Controllers/WorksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShootWeb.Controllers
+{
+    public class WorksController : Controller
+    {
+        //
+        // GET: /Works/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
